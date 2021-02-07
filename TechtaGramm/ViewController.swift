c487d136eed63f4b67a2a8a909f664a3602a688a
sen <ochieeochieeochiee@gmail.com>
2021-02-07 16:14:32 +0900

    Initial Commit

---
+++ TechtaGramm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechtaGramm
+//
+//  Created by Chihiro Nishiwaki on 2021/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
